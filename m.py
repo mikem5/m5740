d1059de6103c606b43814a98b37a7bcfc517238a
mikem5 <mikem5@users.noreply.github.com>
2017-04-15 12:29:10 -0600

    Add files via upload

--- m.py
+++ m.py
@@ -2,21 +2,25 @@ import math
 import random
 import time
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 # global timer
-timer = 0.0
+timer = 0
 maxbooth = 100
 
-# = 2
-alpha = .5
+# = a / .1
+alpha = 2
 
 
-
-ct = []
 cust = []
 active = []
 booths = []
+
+
+def comloc(loc):
+    return loc[0]*10 + loc[1]
 
 
 
@@ -28,11 +32,14 @@ class customer(object):
         self.spending = spending
         self.inqueue = 0
         self.chance = chance
-        self.location = 0
+
+        start_row = np.random.randint(9)
+        self.location = [start_row,0]
         self.wait = wait
-        self.alpha = random.expovariate(alpha)
+        self.alpha = np.random.poisson(alpha)
 
 
+        self.used_rows = [start_row]
 
     def check(self):
         return self.location, self.spending, self.inqueue
@@ -41,33 +48,51 @@ class customer(object):
         self.spending -= 1
         self.inqueue = 0
         if self.spending > 0:
-            self.location += 1
+            self.location_move()
         # left
         else:
-            self.location = maxbooth + 1
+            self.location = [100,100]
+
+
+
+    def location_move(self):
+        if self.location[1] == 9:
+            if len(self.used_rows) >= 10 :
+                self.location = [10,10]
+            else:
+
+                a = [i for i in range(10) if i not in self.used_rows]
+                row = np.random.choice(a)
+                self.used_rows.append(row)
+                self.location[0] = row
+                self.location[1] = 0
+
+        #if self.location[1] == 9:
+        #    self.location[0] += 1
+        #    self.location[1] = 0
+        else:
+            self.location[1]+=1
+ 
 
     def move(self):
         if self.inqueue == 0:
-            if self.alpha <= timer and self.location > 0:
-                self.alpha = timer + random.expovariate(alpha)
+            #if timer >= self.alpha and self.location > [0,0]:
+            #    self.alpha = timer + np.random.poisson(alpha)
                 #chance to enter current line
-                if random.random() < self.chance:
-
-                   booths[self.location].touch()
-                   return True
-                else:
-                    self.location += 1
-                    return False
-            elif self.location == 0:
+            #    if random.random() <= self.chance * booths[self.location[0] + self.location[1]].hotness:
+            #       return True
+            #    else:
+            #        self.location_move() 
+            #        return False
+            #else: #self.location == [0,0]:
                 #chance to enter current line
-                if random.random() < self.chance:
+            if np.random.rand() <= self.chance * booths[comloc(self.location)].hotness:
+#                print comloc(self.location)
+                return True
+            else:
+                self.location_move() 
+                return False
 
-                   booths[self.location].touch()
-                   return True
-                else:
-                    self.location += 1
-                    return False
- 
         else:
             return False
 
@@ -79,15 +104,24 @@ class booth(object):
     def __init__(self,location,br):
         self.location = location
         self.queue = []
-        self.sales = 0.0
+        self.sales = 0
         self.rate = br
         self.oalpha = [0]
         self.lostcust = 0
 
+        # probability mixing with customer probability,
+        # base chance of attracting a customer
+        #
+        # can change distribution for testing?
+#        self.hotness = 1
+        #self.hotness  = np.random.uniform()
+        self.hotness = abs(np.random.normal(0,.1))
+        #self.hotness = np.random.exponential(scale=.1)
+
 
         # time till next serve
-        self.beta = random.expovariate(self.rate)
-
+#        self.beta = np.random.poisson(self.rate)
+        self.beta = 0
 
 
 
@@ -100,14 +134,16 @@ class booth(object):
     def update(self):
         if len(self.queue) > 0:
             # sale occurs
-            if self.beta <= timer:
+            if timer >= 0: #self.beta:
                 self.sales += 1
                 self.queue[0].sale()
                 self.queue.pop(0)
 
 
                 # set next service time
-                self.beta = timer + random.expovariate(self.rate)
+                self.beta = timer + np.random.poisson(self.rate)
+
+
 
     # had a customer 'visit' so add time to oalpha
     def touch(self):
@@ -127,6 +163,7 @@ class booth(object):
                 if si == 0:
                     return float(0.0)
                 else:
+                    #return si
                     return 1/float(si)
 
 
@@ -135,7 +172,7 @@ class booth(object):
 
 
 def inittrial(spending, chance, wait, betarate):
-    global ct, timer
+    global timer
     global cust, active, booths
 
 
@@ -145,29 +182,15 @@ def inittrial(spending, chance, wait, betarate):
     active = []
     booths = []
 
-    # rate at customers appear to booth 0
-    # beta alpha rates would be say 1/rate so 
-    # for .5 = 2 = lambda or .1 = 10
-    # this means avg is 10 'time units' till next event
-
-
-
-    #makes times, this is same as # of customers
-    ct = []
-    t = 0
-    for x in range(100000):
-        t+= random.expovariate(alpha)
-        ct.append(t)
-
-
 
     #make customers
     # customers are formed as cust[[booth location, times to spend], etc]
     for x in range(1000):
         cust.append(customer(x,spending,chance,wait))
 
-    for x in range(100):
-        booths.append(booth(x,betarate))
+    for x in range(10):
+        for y in range(10):
+            booths.append(booth([x,y],betarate))
 
 
 
@@ -175,32 +198,34 @@ def inittrial(spending, chance, wait, betarate):
 
 
 def trial():
-    global timer, ct, booths, cust, active
-    timer = 0.0
+    global timer, booths, cust, active
+    timer = 0
 
+
+    current_alpha = np.random.poisson(alpha)
 
     ci = 0
     while(len(cust)>0 or len(active)>0):
 
 
 
-        for b in booths:
-            b.update()
-
-        if timer >= ci:
+        if timer >= current_alpha:
             if len(cust) > 0:
                 # remove first cust object and put in active list
                 active.append(cust.pop(0))
 
-            if len(ct) > 0:
-                ci = ct.pop(0)
 
+            current_alpha = timer + np.random.poisson(alpha)
+
+
+        for b in booths:
+            b.update()
 
 
         removals= []
 
         for idx,c in enumerate(active):
-            if c.location >= 100:
+            if comloc(c.location) >= 100:
                 removals.append(idx)
 
             elif c.inqueue == 0:
@@ -208,33 +233,52 @@ def trial():
 
                 if c.move() == True:
 
-                    i = c.location
-
+                    i = comloc(c.location)
                     # booth has shorter line 
-                    if len(booths[i].queue) <= c.wait:
-
+                    # just ignore queue for now
+                    #if len(booths[i].queue) >= 0:
+                    if True:
                         booths[i].addcust(c)
                         removals.append(idx)
-
-
+                        booths[i].touch()
+                  
                     # else cust moves on
 
                     else:
-                        c.location += 1
+                        c.location_move()
                         booths[i].lostcust +=1
             else:
                 pass
 
-        for x in sorted(removals,reverse=True):
-            active.pop(x)
+        if len(removals) > 0:
+#            print removals, len(active)
+            for x in sorted(removals,reverse=True):
+#                print x, active[x].location, active[x].inqueue
+                active.pop(x)
+
+
 
 
         timer +=1
 
 
 
+
+
+    for i in range(10):
+        for b in booths:
+            b.update()
+
+        timer +=1
+
+
+
 total_sales = 0
-trial_amt = 50
+
+
+
+# total amount of trials we run
+trial_amt = 1000
 
 avg = [0] * 100
 bavg = [0] * 100
@@ -249,29 +293,31 @@ ax3 = plt.subplot(111)
 
 
 tim = 0
-for brate in [.1,.06,.05,.03,]:
+
+b1 = []
 
 
+for var in [.1,.2,.3,.4,.5]:
 
     for tamt in range(trial_amt):
 
 
         # (# of times to spend, chance to spend, will pass if this many in queue, booth beta)
-        inittrial(1,.1,1,brate)
+        inittrial(1,var,1,1)
 
 
-        s = [ct[i+1] - ct[i] for i in range(len(ct) -1)]
-        s = sum(s) / len(s)
+#        s = [ct[i+1] - ct[i] for i in range(len(ct) -1)]
+#        s = sum(s) / len(s)
 
-        tim = 1/s
+#        tim = 1/s
 
         trial()
 
         for b in booths:
 
-            bavg[b.location]+= b.obsalpha()
-            avg[b.location]+=b.sales
-            lost[b.location]+=b.lostcust
+            bavg[comloc(b.location)]+= b.obsalpha()
+            avg[comloc(b.location)]+=b.sales
+            lost[comloc(b.location)]+=b.lostcust
 
 
 
@@ -279,7 +325,6 @@ for brate in [.1,.06,.05,.03,]:
         #reset
         timer = 0
         maxbooth = 100
-        ct = []
         cust = []
         active = []
         booths = []
@@ -294,15 +339,20 @@ for brate in [.1,.06,.05,.03,]:
         
     for c,d in enumerate(lost):
         lost[c] = d/trial_amt
- 
+
+
+    b1.append([var, avg[0], avg[1], avg[2],avg[3], avg[4], avg[5],sum(avg)])
+
+
+
     x = []
     for i in range(100):
         x.append(i)
 
-    ax1.plot(x,avg,label=brate)
+    ax1.plot(x,avg,label=var)
 
-    ax2.plot(x,bavg,label=brate)
-    ax3.plot(x,lost,label=brate)
+    ax2.plot(x,bavg,label=var)
+    ax3.plot(x,lost,label=var)
 
 
     avg = [0] * 100
@@ -310,16 +360,10 @@ for brate in [.1,.06,.05,.03,]:
     lost = [0]*100
 
 
-t = [0]* 100
-for i in range(100):
-    t[i] = tim
 x = []
 for i in range(100):
     x.append(i)
 
-
-tim = 0
-#ax2.plot(x,t,label='base alpha')
 
 
 plt.figure(fig1.number)
